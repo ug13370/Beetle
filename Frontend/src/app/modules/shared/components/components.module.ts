5eab0161dada02f3e1de93edc2ad745cb57731af
Utkarsh Gupta <utkarsh13370@gmail.com>
2022-10-22 17:12:45 +0530

    Header component setup done

--- Frontend/src/app/modules/shared/components/components.module.ts
+++ Frontend/src/app/modules/shared/components/components.module.ts
@@ -4,11 +4,22 @@ import { MaterialModule } from '../../material/material.module';
 import { BeetleLogoComponent } from './beetle-logo/beetle-logo.component';
 import { BoxComponent } from './box/box.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [BeetleLogoComponent, BoxComponent, SideNavComponent],
+  declarations: [
+    BeetleLogoComponent,
+    BoxComponent,
+    SideNavComponent,
+    HeaderComponent,
+  ],
   imports: [FormsModule, ReactiveFormsModule, MaterialModule],
-  exports: [BeetleLogoComponent, BoxComponent, SideNavComponent],
+  exports: [
+    BeetleLogoComponent,
+    BoxComponent,
+    SideNavComponent,
+    HeaderComponent,
+  ],
   providers: [],
 })
 export class ComponentsModule {}
